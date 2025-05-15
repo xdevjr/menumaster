197fa34f04ef47d1d4433f7487c3e915c884fc6a
JuniorGS <josival1998@gmail.com>
2025-05-14 22:11:27 -0300

    Ajusta o título da página para incluir o nome do aplicativo quando um título é fornecido

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>{{ $title ?? config('app.name') }}</title>
+<title>{{ $title ? $title." | ".config('app.name') : config('app.name') }}</title>
 
 <link rel="icon" href="/favicon.ico" sizes="any">
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
